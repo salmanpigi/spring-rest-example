58018bf8f9c40194b87a76a4ad4a733d36fa5fba
Salman Pigi <C010321004@Supporting.corp.bankmandiri.co.id>
2021-03-08 18:05:52 +0700

    edit fixed

--- src/main/java/id/co/mandiri/day2/mandiriday2/repository/EmployeeRepository.java
+++ src/main/java/id/co/mandiri/day2/mandiriday2/repository/EmployeeRepository.java
@@ -1,5 +1,12 @@
 package id.co.mandiri.day2.mandiriday2.repository;
 
-public interface EmployeeRepository {
+import id.co.mandiri.day2.mandiriday2.model.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    Optional<Employee> findByEmployeeId(Integer employeeId);
 }
